0efbb6f14132665d03b8e88fe05e231dd4860a20
Felipe Escobedo <escobedo.felipe@hotmail.com>
2020-05-06 18:23:27 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    database: 'localhost',
+    user: 'root',
+    password: '4dm1n.p05',
+    database: 'test'
+}
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+
+router.get('/', (req, res) => {
+    
+});
+
+module.exports = router;
